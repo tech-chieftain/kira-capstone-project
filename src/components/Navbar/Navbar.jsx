862c45340d2d33a233733841662f42e62323d898
Salah Al-Dhaferi <binaryminded@gmail.com>
2021-07-01 03:09:16 +0300

    added react bootstrap

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button } from "antd";
+import Button from "react-bootstrap/Button";
 import { Header } from "./Navbar.styled";
 
 const Navbar = () => (
   <div>
     <p>This is an example of a component</p>
-    <Button type="primary">Example of Antd button</Button>
+    <Button variant="primary">Example of Antd button</Button>
     <Header>WELCOME</Header>
   </div>
 );
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import { i18n } from "next-i18next";
-import "antd/dist/antd.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 // eslint-disable-next-line react/prop-types
 function App({ Component, pageProps }) {
@@ -10,6 +11,13 @@ function App({ Component, pageProps }) {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default appWithTranslation(App);
