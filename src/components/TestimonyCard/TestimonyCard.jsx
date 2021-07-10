1cad7d7bb6b69d2290a4618a9cbe3646aa4c4888
HasarAli <Hasar.Ali@protonmail.com>
2021-07-10 19:07:41 +0300

    added test and reformatted

--- src/components/TestimonyCard/TestimonyCard.jsx
+++ src/components/TestimonyCard/TestimonyCard.jsx
@@ -1,19 +1,18 @@
-import React from "react";
+import PropTypes from "prop-types";
 
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-import Rating from 'react-rating'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar as fasStar, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import Rating from "react-rating";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as fasStar, faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
 
-const TestimonyCard = ({imgSrc, quote, stars, color}) => (
+const TestimonyCard = ({ imgSrc, quote, rating, color }) => (
   <Card>
-    <Row >
+    <Row>
       <Col>
         <Card.Img variant="top" src={imgSrc} />
       </Col>
@@ -21,16 +20,16 @@ const TestimonyCard = ({imgSrc, quote, stars, color}) => (
         <Card.Body>
           <Card.Text className="text-center">
             <div className="text-start">
-              <FontAwesomeIcon icon={faQuoteLeft} color={color} size="2x"/>
+              <FontAwesomeIcon icon={faQuoteLeft} color={color} size="2x" />
             </div>
             {quote}
             <div className="text-end">
-              <FontAwesomeIcon icon={faQuoteRight} color={color} size="2x"/>
+              <FontAwesomeIcon icon={faQuoteRight} color={color} size="2x" />
             </div>
           </Card.Text>
           <Rating
             readonly
-            initialRating={stars}
+            initialRating={rating}
             emptySymbol={<FontAwesomeIcon icon={farStar} />}
             fullSymbol={<FontAwesomeIcon icon={fasStar} />}
             fractions={2}
@@ -41,11 +40,18 @@ const TestimonyCard = ({imgSrc, quote, stars, color}) => (
   </Card>
 );
 
+TestimonyCard.propTypes = {
+  imgSrc: PropTypes.string,
+  quote: PropTypes.string,
+  rating: PropTypes.number,
+  color: PropTypes.string,
+};
+
 TestimonyCard.defaultProps = {
   imgSrc: "https://picsum.photos/300/200",
-  quote: "To be, or not to be?",
-  stars: 4,
-  color: "green",
-}
+  quote: "",
+  rating: 4.5,
+  color: "",
+};
 
 export default TestimonyCard;
