e7814b591c8c6f4cada530f71fde7a341ea2cf07
HasarAli <Hasar.Ali@protonmail.com>
2021-07-16 17:57:24 +0300

    replaced FA with react-icons

--- src/components/SmallProfileCard/SmallProfileCard.jsx
+++ src/components/SmallProfileCard/SmallProfileCard.jsx
@@ -5,19 +5,14 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { FaUserCircle } from "react-icons/fa";
 
 const SmallProfileCard = ({ profilePicture, name, job }) => (
   <Card>
     <Card.Body>
       <Row>
         <Col xs={4} className="align-self-center">
-          {profilePicture ? (
-            <Image src={profilePicture} roundedCircle fluid />
-          ) : (
-            <FontAwesomeIcon icon={faUserCircle} size="3x" />
-          )}
+          {profilePicture ? <Image src={profilePicture} roundedCircle fluid /> : <FaUserCircle />}
         </Col>
         <Col>
           <Card.Title>{name}</Card.Title>
