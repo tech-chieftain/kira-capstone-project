50d38d705b44f0ec15a5cccce7b462084769ecf1
HasarAli <hasar.ali@protonmail.com>
2021-07-20 22:14:16 +0300

    changed props & made thumbnails optional

--- src/components/CustomCarousel/CustomCarousel.jsx
+++ src/components/CustomCarousel/CustomCarousel.jsx
@@ -3,47 +3,40 @@ import PropTypes from "prop-types";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const CustomCarousel = ({ thumbnail, images, captions }) => {
+const CustomCarousel = ({ thumbnail, items, slides }) => {
   const settings = {
-    customPaging(i) {
-      return (
-        <a>
-          <img src={images[i]} alt={`${i} slide`} style={{ height: "100%", objectFit: "100%" }} />
-        </a>
-      );
-    },
     dots: true,
     dotsClass: "slick-dots slick-thumb",
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: slides,
     slidesToScroll: 1,
     nextArrow: (
-      <img
-        src="https://picsum.photos/100/100?random=1"
-        alt="next"
-        style={{ height: "100%", objectFit: "100%" }}
-      />
+      <div>
+        <img
+          src="https://picsum.photos/100/100?random=1"
+          alt=""
+          style={{ height: "100%", objectFit: "100%" }}
+        />
+      </div>
     ),
     prevArrow: (
-      <img
-        src="https://picsum.photos/100/100?random=2"
-        alt="previous"
-        style={{ height: "100%", objectFit: "100%" }}
-      />
+      <div>
+        <img
+          src="https://picsum.photos/100/100?random=2"
+          alt=""
+          style={{ height: "100%", objectFit: "100%" }}
+        />
+      </div>
     ),
   };
+
+  if (thumbnail) settings.customPaging = (i) => <a>{items[i]}</a>;
+
   return (
     <Slider {...settings} className="container">
-      {images.map((image) => (
-        <div>
-          <img
-            className="m-auto"
-            src={image}
-            alt=""
-            style={{ height: "100%", objectFit: "100%" }}
-          />
-        </div>
+      {items.map((item) => (
+        <div>{item}</div>
       ))}
     </Slider>
   );
@@ -51,18 +44,14 @@ const CustomCarousel = ({ thumbnail, images, captions }) => {
 
 CustomCarousel.propTypes = {
   thumbnail: PropTypes.bool,
-  images: PropTypes.array,
-  captions: PropTypes.array,
+  items: PropTypes.array,
+  slides: PropTypes.number,
 };
 
 CustomCarousel.defaultProps = {
   thumbnail: false,
-  images: [
-    "https://picsum.photos/300/200?random=1",
-    "https://picsum.photos/300/200?random=2",
-    "https://picsum.photos/300/200?random=3",
-  ],
-  captions: [],
+  items: [],
+  slides: 3,
 };
 
 export default CustomCarousel;
