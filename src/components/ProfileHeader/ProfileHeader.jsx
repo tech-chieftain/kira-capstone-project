649646de73189c6997afdc927d32492a3ff9bc97
Kizhan-Mohammed-karim <kizhan.m.kareem@gmail.com>
2021-07-25 16:06:27 +0300

    add background image, user profile image and  social media

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,33 +1,85 @@
-import { Background } from "./ProfileHeader.styled";
 import PropTypes from "prop-types";
+import { Row, Col } from "react-bootstrap";
+import { FaUserCircle, FaTwitter, FaInstagram, FaFacebookF } from "react-icons/fa";
+import { VscLocation } from "react-icons/vsc";
+import Link from "next/link";
+import { Background, Img, MainContainer } from "./ProfileHeader.styled";
 
 const ProfileHeader = ({
   profilePicture,
   name,
-  profession,
+  //   profession,
   address,
   description,
-  rating,
+  //   rating,
+  // eslint-disable-next-line arrow-body-style
 }) => {
-  return <Background></Background>;
+  return (
+    <Background>
+      <MainContainer>
+        <Row>
+          <Col sm={3}>
+            {profilePicture ? (
+              <Img src={profilePicture} roundedCircle fluid />
+            ) : (
+              <FaUserCircle size="173px" />
+            )}
+          </Col>
+          <Col sm={8} className="info-container">
+            <h1>{name}</h1>
+            <h5>
+              <VscLocation size="32px" />
+              <span>{address}</span>
+            </h5>
+            <div>
+              <ul className="list-unstyled d-flex flex-row">
+                <li>
+                  <Link href="/">
+                    <a>
+                      <FaFacebookF size="22px" />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a>
+                      <FaInstagram size="22px" />
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a>
+                      <FaTwitter size="22px" />
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <p>{description}</p>
+          </Col>
+        </Row>
+      </MainContainer>
+    </Background>
+  );
 };
 
 ProfileHeader.propTypes = {
   profilePicture: PropTypes.string,
   name: PropTypes.string,
-  profession: PropTypes.string,
+  //   profession: PropTypes.string,
   address: PropTypes.string,
   description: PropTypes.string,
-  rating: PropTypes.number,
+  //   rating: PropTypes.number,
 };
 
 ProfileHeader.defaultProps = {
   profilePicture: "",
   name: "",
-  profession: "",
+  //   profession: "",
   address: "",
   description: "",
-  rating: ,
+  //   rating: ,
 };
 
 export default ProfileHeader;
--- src/components/ProfileHeader/ProfileHeader.styled.jsx
+++ src/components/ProfileHeader/ProfileHeader.styled.jsx
@@ -1,13 +1,35 @@
 import styled from "styled-components";
+import { Image, Container } from "react-bootstrap";
 
 export const Background = styled.main`
   width: 100%;
   height: 426px;
-  background: linear-gradient(
-    to right,
-    rgba(0, 78, 79, 0.5304),
-    rgba(0, 128, 128, 0.6948),
-    rgba(0, 98, 98, 0.2736)
-  );
+  background-image: url(/background.png);
+  background-repeat: no-repeat;
+  background-size: cover;
   color: #fff;
+  display: flex;
+  align-items: center;
+`;
+
+export const Img = styled(Image)`
+  height: 173px;
+  width: 173px;
+  overflow: hidden;
+`;
+
+export const MainContainer = styled(Container)`
+  width: 50%;
+  height: 70%;
+  margin-left: 12rem;
+  margin-top: 2rem;
+
+  .info-container {
+    margin-top: -1.5rem;
+
+    li a {
+      color: white;
+      margin-right: 6px;
+    }
+  }
 `;
