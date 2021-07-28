89d41cd5b44b59ea5882fc998c1eed8242c24036
HasarAli <Hasar.Ali@protonmail.com>
2021-07-28 22:54:31 +0300

    added placeholders

--- src/pages/freelancers_search/index.jsx
+++ src/pages/freelancers_search/index.jsx
@@ -1,15 +1,38 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
-import Navbar from "../../components/Navbar/Navbar"
 
-const freelancers = () => (
-  <>
-    <Head>
-      <title>Freelancer Search</title>
-    </Head>
-    <Navbar />
-  </>
-);
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import Navbar from "../../components/Navbar/Navbar";
+import Pagination from "../../components/Pagination/Pagination";
+
+const freelancers = ({ query }) => {
+  const pageCount = 10;
+  const handlePageClick = (selectedPage) => "";
+  return (
+    <>
+      <Head>
+        <title>Freelancer Search</title>
+      </Head>
+      <Container>
+        <Row>
+          <Col>
+            <h3>Results for "{query}"</h3>
+          </Col>
+          <Col>placeholder for dropdowns</Col>
+        </Row>
+        <Row>
+          <Col>placeholder for cards</Col>
+        </Row>
+        <Row>
+          <Pagination pageCount={pageCount} handlePageClick={handlePageClick} />
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
