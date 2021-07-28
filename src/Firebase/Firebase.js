80f8e947899e0637651b8de71e30b5167b11f723
dlzheen <dlzheenmahmood@gmail.com>
2021-07-28 01:50:15 -0700

    changed firebase account

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -4,12 +4,12 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyD8p3sWlX_ef2-yrEGwnzv4ekPlqQawhqQ",
-    authDomain: "kira-freelance-app.firebaseapp.com",
-    projectId: "kira-freelance-app",
-    storageBucket: "kira-freelance-app.appspot.com",
-    messagingSenderId: "734137959084",
-    appId: "1:734137959084:web:cb0f308db3947ca6d09509"
+  apiKey: "AIzaSyADigYReEaAkwaTOMUT3n-0DdHxgThzk94",
+  authDomain: "kira-freelancer-app.firebaseapp.com",
+  projectId: "kira-freelancer-app",
+  storageBucket: "kira-freelancer-app.appspot.com",
+  messagingSenderId: "599866219282",
+  appId: "1:599866219282:web:1b5f10c7d88ddc4736f1cb"
 };
 // Initialize Firebase
 !Firebase.apps.length ? Firebase.initializeApp(firebaseConfig) : Firebase.app();
