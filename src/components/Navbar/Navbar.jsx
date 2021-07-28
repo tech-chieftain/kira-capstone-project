6e46a20356eb1e7d1ade6c1b24e167950ef9790b
Kizhan-Mohammed-karim <kizhan.m.kareem@gmail.com>
2021-07-29 01:58:54 +0300

    added the second version of the navbar and the user profile dropdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { LoginBtn, JoinBtn, Img } from "./Navbar.styled";
+import { Navbar, Nav, Container, Dropdown } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { FaUserCircle, FaUserAlt } from "react-icons/fa";
+import { IoIosNotifications } from "react-icons/io";
+import { GrMail } from "react-icons/gr";
+import { VscSearch } from "react-icons/vsc";
+import { LoginBtn, JoinBtn, Img, ProfileImg, DropDown } from "./Navbar.styled";
 import LoginModal from "../Modals/LoginModal";
 import SignupModal from "../Modals/SignupModal";
 
 // eslint-disable-next-line arrow-body-style
-const NavBar = () => {
+const NavBar = ({ overview, profilePicture }) => {
   const [showLogin, setShowLogin] = useState(false);
   const [showSignup, setShowSignup] = useState(false);
 
@@ -29,24 +34,66 @@ const NavBar = () => {
             alt="Nabar logo"
           />
         </Navbar.Brand>
-        <Nav>
-          <LoginBtn onClick={openLoginModal}> Log In </LoginBtn>
-          <LoginModal
-            showLogin={showLogin}
-            setShowLogin={setShowLogin}
-            setShowSignup={setShowSignup}
-          />
 
-          <JoinBtn onClick={openSignupModal}> Join </JoinBtn>
-          <SignupModal
-            showSignup={showSignup}
-            setShowSignup={setShowSignup}
-            setShowLogin={setShowLogin}
-          />
-        </Nav>
+        {overview ? (
+          <Nav>
+            <LoginBtn onClick={openLoginModal}> Log In </LoginBtn>
+            <LoginModal
+              showLogin={showLogin}
+              setShowLogin={setShowLogin}
+              setShowSignup={setShowSignup}
+            />
+
+            <JoinBtn onClick={openSignupModal}> Join </JoinBtn>
+            <SignupModal
+              showSignup={showSignup}
+              setShowSignup={setShowSignup}
+              setShowLogin={setShowLogin}
+            />
+          </Nav>
+        ) : (
+          <Nav>
+            <Nav.Link href="/">
+              <VscSearch size="28px" className="text-white mt-1" />
+            </Nav.Link>
+            <Nav.Link href="/">
+              <GrMail size="32px" className="text-white" />
+            </Nav.Link>
+            <Nav.Link href="/">
+              <IoIosNotifications size="32px" className="text-white" />
+            </Nav.Link>
+
+            <DropDown>
+              <Dropdown.Toggle className="dropdown-basic">
+                <FaUserAlt size="25px" className="text-white" />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="/">
+                  {profilePicture ? (
+                    <ProfileImg src={profilePicture} roundedCircle fluid />
+                  ) : (
+                    <FaUserCircle size="28px" />
+                  )}
+                </Dropdown.Item>
+                <Dropdown.Item href="/" />
+              </Dropdown.Menu>
+            </DropDown>
+          </Nav>
+        )}
       </Container>
     </Navbar>
   );
+};
+
+NavBar.propTypes = {
+  overview: PropTypes.bool,
+  profilePicture: PropTypes.string,
+};
+
+NavBar.defaultProps = {
+  overview: false,
+  profilePicture: "",
 };
 
 export default NavBar;
--- src/components/Navbar/Navbar.styled.jsx
+++ src/components/Navbar/Navbar.styled.jsx
@@ -1,9 +1,5 @@
 import styled from "styled-components";
-import { Button, Image } from "react-bootstrap";
-
-export const Img = styled(Image)`
-  margin: -8px 0;
-`;
+import { Button, Image, Dropdown } from "react-bootstrap";
 
 export const JoinBtn = styled(Button)`
   height: 32px;
@@ -13,11 +9,11 @@ export const JoinBtn = styled(Button)`
   border-radius: 5px;
   border: none;
 
-  &:focus,
-  :hover {
-    color: #006767;
-    background: #fff;
-  }
+  // &:focus,
+  // :hover {
+  //   color: #006767;
+  //   background: #fff;
+  // }
 `;
 
 export const LoginBtn = styled(Button)`
@@ -29,13 +25,36 @@ export const LoginBtn = styled(Button)`
   border: none;
   margin-right: 75px;
 
-  &:focus,
-  :hover {
-    color: #fff;
-    background: #008080;
-  }
+  // &:focus,
+  // :hover {
+  //   color: #fff;
+  //   background: #008080;
+  // }
 
   @media (max-width: 768px) {
     margin-right: 20px;
+  }
+`;
+
+export const Img = styled(Image)`
+  margin: -8px 0;
+`;
+
+export const ProfileImg = styled(Image)`
+  width: 28px;
+  height: 28px;
+  overflow: hidden;
+`;
+
+export const DropDown = styled(Dropdown)`
+  .dropdown-basic {
+    color: white;
+    box-shadow: none;
+    webkit-box-shadow: none;
+    margin: 3px auto 0;
+
+    // &::after {
+    //   display: none;
+    // }
   }
 `;
