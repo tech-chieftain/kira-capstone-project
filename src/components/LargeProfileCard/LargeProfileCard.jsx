9b99ff33d4c4d15ae5666cfac4334ab0f5cee33d
HasarAli <Hasar.Ali@protonmail.com>
2021-07-29 03:14:59 +0300

    added username prop and interactive pp, name, and tags

--- src/components/LargeProfileCard/LargeProfileCard.jsx
+++ src/components/LargeProfileCard/LargeProfileCard.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
 
+import Link from "next/link";
+
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,6 +11,7 @@ import Button from "react-bootstrap/Button";
 import { FaUserCircle, FaStar } from "react-icons/fa";
 
 const LargeProfileCard = ({
+  username,
   profilePicture,
   name,
   job,
@@ -22,17 +25,29 @@ const LargeProfileCard = ({
     <Card.Body>
       <Row className="align-items-center">
         <Col xs={4}>
-          {profilePicture ? (
-            <Image src={profilePicture} alt="" roundedCircle fluid />
-          ) : (
-            <div className="d-flex justify-content-end">
-              <FaUserCircle size="4em" />
-            </div>
-          )}
+          <Link href="/user/[]" as={`/user/${username}`}>
+            <a>
+              {profilePicture ? (
+                <Image src={profilePicture} alt="" roundedCircle fluid />
+              ) : (
+                <div className="d-flex justify-content-end">
+                  <FaUserCircle size="4em" />
+                </div>
+              )}
+            </a>
+          </Link>
         </Col>
         <Col>
-          <Card.Title className="text-primary">{name}</Card.Title>
-          <Card.Subtitle>{job}</Card.Subtitle>
+          <Link href="/user/[]" as={`/user/${username}`}>
+            <a className="text-decoration-none text-reset">
+              <Card.Title className="text-primary">{name}</Card.Title>
+            </a>
+          </Link>
+          <Link href="/services_search?q=[]" as={`/services_search?q=${job}`}>
+            <a className="text-decoration-none text-reset">
+              <Card.Subtitle>{job}</Card.Subtitle>
+            </a>
+          </Link>
           <Card.Text className="fw-light">{location}</Card.Text>
         </Col>
       </Row>
@@ -44,16 +59,17 @@ const LargeProfileCard = ({
       <Row>
         <Col>
           {tags.map((tag) => (
-            <Button variant="outline-primary me-2 mb-2">{tag}</Button>
+            <Link href="/services_search?q=[]" as={`/services_search?q=${tag}`}>
+              <a className="text-decoration-none text-reset">
+                <Button variant="outline-primary me-2 mb-2">{tag}</Button>
+              </a>
+            </Link>
           ))}
         </Col>
       </Row>
       <hr />
       <Row>
-        <Col>
-          <Card.Text>${rate}/hr</Card.Text>
-        </Col>
-        <Col className="text-end">
+        <Col className="text-end fluid">
           {topRated && (
             <Card.Text className="d-flex">
               <FaStar size="1rem" className="align-self-center m-1" /> Top Rated
@@ -66,6 +82,7 @@ const LargeProfileCard = ({
 );
 
 LargeProfileCard.propTypes = {
+  username: PropTypes.string,
   profilePicture: PropTypes.string,
   name: PropTypes.string.isRequired,
   job: PropTypes.string,
@@ -77,12 +94,12 @@ LargeProfileCard.propTypes = {
 };
 
 LargeProfileCard.defaultProps = {
+  username: "johndoe1",
   profilePicture: "https://picsum.photos/100/100",
   name: "John Doe",
   job: "Web dev",
   location: "Toronto",
-  about:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quidem.",
+  about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, quidem.",
   tags: ["wordpress", "front end dev", "gaming", "catering"],
   rate: 50,
   topRated: true,
