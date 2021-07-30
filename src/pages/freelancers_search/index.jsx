62c99065a3f9f5bac15131477a7b9db4b50aaa9c
HasarAli <Hasar.Ali@protonmail.com>
2021-07-30 03:13:16 +0300

    added dropdown

--- src/pages/freelancers_search/index.jsx
+++ src/pages/freelancers_search/index.jsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 import Navbar from "../../components/Navbar/Navbar";
 import Pagination from "../../components/Pagination/Pagination";
@@ -21,7 +23,13 @@ const freelancers = ({ query }) => {
           <Col>
             <h3>Results for "{query}"</h3>
           </Col>
-          <Col>placeholder for dropdowns</Col>
+          <Col className="d-flex justify-content-end">
+            <DropdownButton title="Sort by">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+          </Col>
         </Row>
         <Row>
           <Col>placeholder for cards</Col>
