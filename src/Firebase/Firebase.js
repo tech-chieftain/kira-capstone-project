463f3f65ea6a14eb8d7e7a25ae64ab9e7baa0636
HasarAli <Hasar.Ali@protonmail.com>
2021-07-31 20:36:39 +0300

    added environment variables for security

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,17 +1,20 @@
-import * as Firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyADigYReEaAkwaTOMUT3n-0DdHxgThzk94",
-  authDomain: "kira-freelancer-app.firebaseapp.com",
-  projectId: "kira-freelancer-app",
-  storageBucket: "kira-freelancer-app.appspot.com",
-  messagingSenderId: "599866219282",
-  appId: "1:599866219282:web:1b5f10c7d88ddc4736f1cb"
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 // Initialize Firebase
-!Firebase.apps.length ? Firebase.initializeApp(firebaseConfig) : Firebase.app();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-export default Firebase;
+export default firebase;
