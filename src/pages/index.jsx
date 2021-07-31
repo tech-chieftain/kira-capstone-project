32ec4a29d1eb13fdadbd9478ea4fcef687f4fd2e
HasarAli <Hasar.Ali@protonmail.com>
2021-07-31 20:39:23 +0300

    testing stuff

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "../Firebase";
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Redirect to / after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display GitHub as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
 function Homepage() {
   // This controls the direction of the page for RTL languages
   const { t } = useTranslation("common");
 
-  return <div />;
+  return (
+    <>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </>
+  );
 }
 
 // This function below should exist in everypage
