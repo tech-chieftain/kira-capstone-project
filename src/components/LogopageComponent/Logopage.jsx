ae57e67e485c7e5703228ea056d979de4ec7992f
Raghda Muayad <therose000777@gmail.com>
2021-07-31 22:54:16 +0300

    Created The Try Kira Logo Service Section

--- src/components/LogopageComponent/Logopage.jsx
+++ src/components/LogopageComponent/Logopage.jsx
@@ -1,66 +1,114 @@
-import PropTypes from "prop-types";
-import { Card, Col, Row } from "react-bootstrap";
-import { AiOutlineHistory} from "react-icons/ai";
-import { ModalBubbles,MainContainer, Button,Container } from "./Logopage.styled";
-import { FcCheckmark } from "react-icons/fc";
-import { VscRefresh } from "react-icons/vsc";
+import React from "react";
+import Head from "next/head";
+import Image from 'next/image';
+import Link from "next/link";
+import Card from 'react-bootstrap/Card'
+import { Header } from "./Logopage.styled";
+import { Container, Row} from "react-bootstrap";
+import {  Background,MianContainer,Button,Col,ModalBubbles,Form,Footer} from "./Logopage.styled";
 
 
-const Price = ({ price, data, date,revisions }) => (
+ 
 
-   <Container>
+ 
+ 
+ const Logopage = () => {
+  
+  
+     <div>
+        <Head>
+           <title> logo</title>
+        </Head>
+    </div>
      
-    <Card.Body style={{ width: "50rem" }}>
-       <Row>
-    <ModalBubbles>
-        
-        <Card.Title><span>Price</span><small className="mx-3"className="price">
-             <b>$</b> {price}
-          
-          </small></Card.Title>
+  return (
+  
+  <Background>                
+       <MianContainer >
+          <Link href="/">
+                   <Image
+                       width={190}
+                          height={170}    
+                            sizes="(max-width: 200px) 100px"
+                         src="/logoimg1.png"
+                      alt="image"
+                    fluid={true}
+                   />    
+           </Link>
+       </MianContainer >  
+       <Footer>
+       <Link href="/">
+                   <Image
+                       width={160}
+                          height={150}    
+                            sizes="(max-width: 200px) 100px"
+                         src="/logoimg3.png"
+                      alt="image"
+                    fluid={true}
+                   />    
+            </Link>
+         </Footer> 
+       <Col>
+         <Link href="/">
+                   <Image
+                       width={160}
+                          height={150}    
+                            sizes="(max-width: 200px) 100px"
+                         src="/logoimg5.png"
+                      alt="image"
+                    fluid={true}
+                  />    
+               </Link >   
+             </Col>
+             <Form>  
+     <Link href="/"className="logo">
+          <Image
+                width={130}
+                     height={130}    
+                            sizes="(max-width: 200px) 100px"
+                         src="/logoimg6.png"
+                      alt="image"
+                    fluid={true}
+                   />    
+               </Link>
+               </Form>  
+           <ModalBubbles>
+           <Link href="/" className="newlogo">
+                   <Image
+                       width={100}
+                          height={100}    
+                            sizes="(max-width: 200px) 100px"
+                         src="/img22.png"
+                      alt="image"
+                    fluid={true}
+                   />    
+               </Link>
+           </ModalBubbles>
+                <Container>
+                   <Row xs={3} md={2}>
+                          <Header>
+                              <Card.Title> 
+                                         <h2><div className="Header"></div> <b>Make an incredible logo</b></h2>
+                                      </Card.Title>
+                                       <Card.Title>
+                                           <h5 className="text">Design your logo by top talents</h5>
+                                    </Card.Title>
+                           </Header>
+                        </Row>
+                 </Container>
+           <Row>
+              <Button  className="button">     
+                          Try Kira Logo Services
+                </Button>
+           </Row>
+      
+ </Background>
+   );
    
-      
-        </ModalBubbles>
-        </Row>
-      
-      <Card.Text className="mx-3">{data}</Card.Text>
-      
-   
-      <small className="mx-2 text-muted">   <  AiOutlineHistory /> {date} Day Delivery </small>
-      <small className="mx-5 text-muted">   <  VscRefresh /> {revisions} Revisions </small>
-   < MainContainer>
-      <Row>
-      <small className="mx-1 text-muted">  <FcCheckmark /> {data} </small>
-      </Row>
-      <Row>
-      <small className="mx-1 text-muted">  <FcCheckmark />  {data} </small>
-      </Row>
-      
-      <small className="mx-1 text-muted">  <FcCheckmark />  {data}</small>
-      </MainContainer>
-     <Col>
-   
-   <Button>
-   Continue( $ {price}  )
-   </Button>
-   </Col>
-   <Col>
-   <Button className="seller">
-     Contact Seller
-   </Button>
-   </Col>
+ };
+ 
+ 
+ 
 
-      
-     
-    </Card.Body>
-  </Container>
-);
+export default Logopage;
 
-Price.propTypes = {
-  data: PropTypes.string,
-  price: PropTypes.number,
-  revisions: PropTypes.number,
-  date: PropTypes.string,
-};
-
-export default Price;
--- src/components/LogopageComponent/Logopage.styled.jsx
+++ src/components/LogopageComponent/Logopage.styled.jsx
@@ -1,53 +1,137 @@
+/* eslint-disable import/prefer-default-export */
+
 import styled from "styled-components";
-//import { FcCheckmark } from "react-icons/fc";
-
-export const Container = styled.div`
-  border: 1px solid rgba(1, 1, 1, 1);
-  width:20rem;
-  height:25rem;
-  margin: 1px 555px;
-  box-shadow: 0 6px 5px 0 rgba(0, 0, 0, 0), 0 5px 6px 0 rgba(0, 0, 0, 0.1);
-`;
-export const MainContainer = styled.div`
-
-margin: 50px 10px;
-
-   
-`;
 
 
-export const ModalBubbles= styled.div`
+export const Background = styled.div`
+ 
+background: #ffffff;
+width: 100%;
+height: 514px;
+position: fixed;
+bottom: 1;
+left: 0;
+margin: 0;
+margin: 0px 0px;
+box-shadow: 0 8px 6px 0 rgba(0, 0, 0, 0), 0 6px 5px 0 rgba(0, 0, 0, 0.1);
 
-margin: 0px 25px;
-
-.price {
-     margin: 1px 190px;
-
-   }
-   
 `;
 
+export const Button = styled.div`
 
-export const Button= styled.button`
-
-width: 32%;
-height: 50px;
-background: #008080;
-color:white;
+position: absolute;
+left: 0px;
+border-radius: 0px 0px 0px 0px;
+font-size: 18px;
+background-color: #ffffff;
+ width: 22%;
+highte: 5%;
+color:black;
+border: 1px solid #008080;
+padding: 10px 35px;
 text-align: center;
 text-decoration: none;
-margin: 100px 30px;
+margin: 40px 106px;
 transition-duration: 0.4s;
 cursor: pointer;
-border: 1px solid #008080;
-text-shadow: 1px 1px 1px #3d3d5c;
+ text-shadow: 1px 1px 1px #3d3d5c;
 box-shadow: 0 6px 5px 0 rgba(0, 0, 0, 0), 0 3px 2px 0 rgba(0, 0, 0, 0.1);
-
-  &:hover {
-   color:#008080;
-   background-color: #ffffff;
-             
-    }
-
-  
+        
+&:hover {
+ border: black;
+ background-color: #008080;
+           
+        }
+    
 `;
+
+
+export const Header = styled.div`
+
+margin: 130px 0px;
+margin-bottom: 0px;
+.text {
+font-style: italic;
+font-weight: 100;
+   
+span {
+font-style: normal;
+font-weight: bold;
+}
+   
+
+`;
+
+export const MianContainer = styled.div`
+
+width: 190px;
+height: 170px;
+color: #ffffff;
+position: fixed;
+margin: 90px 490px;
+cursor: pointer;
+right: 0;
+bottom: 1;
+top: 100;
+Z-index: 1;
+   `;
+export const Col = styled.div`
+
+width: 190px;
+height: 170px;
+color: #ffffff;
+position: fixed;
+margin: 340px 255px;
+cursor: pointer;
+right: 0;
+bottom: 0;
+top: 100;
+Z-index: 1;
+     
+   `;
+
+export const ModalBubbles = styled.div`
+
+width: 240px;
+height: 200px;
+color: #ffffff;
+position: fixed;
+margin: 60px 0px;
+cursor: pointer;
+right: 0;
+bottom: 1;
+top: 100;
+Z-index: 1;
+   `;
+export const Footer = styled.div`
+
+width: 150px;
+height: 170px;
+color: #ffffff;
+position: fixed;
+margin: 280px 380px;
+cursor: pointer;
+right: 0;
+bottom: 1;
+top: 100;
+Z-index: 1;
+   `;
+
+export const Form = styled.div`
+
+width: 130px;
+height: 130px;
+color: #ffffff;
+position: fixed;
+margin: 290px 170px;
+cursor: pointer;
+right: 0;
+bottom: 1;
+top: 100;
+Z-index: 1;
+   `;
+
+
+
+
+
