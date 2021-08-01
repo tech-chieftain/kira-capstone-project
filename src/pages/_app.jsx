20e9d3e93c68f759872b03c1749a23e8bab02665
HasarAli <Hasar.Ali@protonmail.com>
2021-08-01 12:18:30 +0300

    navbar type reflects user status

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
 
+import { useAuthState } from "react-firebase-hooks/auth";
 import { appWithTranslation, i18n } from "next-i18next";
+import firebase from "../Firebase";
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -9,20 +11,26 @@ import "../styles/scss/global.scss";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/Navbar/Navbar";
 
-
 // eslint-disable-next-line react/prop-types
 function App({ Component, pageProps }) {
   useEffect(() => {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
+  const [user, loading] = useAuthState(firebase.auth());
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      
-      <NavBar />
+
+      <NavBar
+        overview={!user || loading}
+        name={user && user.displayName}
+        profilePicture={user && user.photoURL}
+        handleLogOut={() => user && firebase.auth().signOut()}
+      />
       <Component {...pageProps} />
       <Footer />
     </>
