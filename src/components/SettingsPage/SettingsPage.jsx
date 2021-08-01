a1dbfe03926455fac5fec19d0dd6bb9cd62b4b18
Kizhan-Mohammed-karim <kizhan.m.kareem@gmail.com>
2021-08-02 01:34:09 +0300

    changing the two forms into one

--- src/components/SettingsPage/SettingsPage.jsx
+++ src/components/SettingsPage/SettingsPage.jsx
@@ -1,40 +1,125 @@
-import { Card, Col, Row } from "react-bootstrap";
+/* eslint-disable arrow-body-style */
+import { Card, Col, Row, Form, Button } from "react-bootstrap";
 import React, { useState } from "react";
-import PersonalInfo from "./PersonalInfo";
-import ProfessionalInfo from "./ProfessionalInfo";
-import { Container, MainContainer, Button } from "./SettingsPage.styled";
+import { Container, MainContainer } from "./SettingsPage.styled";
 
 const SettingsPage = () => {
-  const [showPersonal, setShowPersonal] = useState(true);
-  const [showProfessional, setShowProfessional] = useState(false);
+  const [skills, setSkills] = useState([]);
+  const [education, setEducation] = useState([]);
+  const [languages, setLanguages] = useState([]);
 
-  const openPersonal = () => {
-    setShowPersonal(true);
-    setShowProfessional(false);
+  const handleChange = (set) => (e) => {
+    set(e.target.value.split(", "));
   };
 
-  const openProfessional = () => {
-    setShowProfessional(true);
-    setShowPersonal(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
+  const arrayToString = (arr) => arr.join(", ");
   return (
-    <Container>
+    <Container className="d-flex flex-column justify-content-center align-items-center">
       <MainContainer>
-        <Row className="p-5">
-          <Col md={4}>
-            <Button onClick={openPersonal}>Personal Information</Button>
-            <Button onClick={openProfessional}>Professional Information</Button>
-          </Col>
-          <Col>
-            <Card className="card shadow">
-              <Card.Body>
-                <PersonalInfo showPersonal={showPersonal} />
-                <ProfessionalInfo showProfessional={showProfessional} />
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <h2 className="text-primary m-3">Account Settings</h2>
+        <Card className="shadow">
+          <Card.Body>
+            <Form onSubmit={handleSubmit}>
+              <Card.Header className="text-muted bg-white h4 mb-4 header">
+                Personal Information
+              </Card.Header>
+
+              <Row className="mb-4">
+                <Form.Group as={Col}>
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control placeholder="first Name" />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control placeholder="last Name" />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-4">
+                <Form.Group as={Col}>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control placeholder="username" />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control placeholder="location" />
+                </Form.Group>
+              </Row>
+
+              <Row className="mb-4">
+                <Form.Group as={Col}>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control placeholder="email" />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label>Phone number</Form.Label>
+                  <Form.Control placeholder="phone number" />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-5">
+                <Form.Label>Change Profile Picture</Form.Label>
+                <Form.Control placeholder="profile picture link" />
+              </Form.Group>
+
+              <Card.Header className="text-muted bg-white h4 my-4 header">
+                Professional Information
+              </Card.Header>
+
+              <Form.Group className="mb-4">
+                <Form.Label>Jobs</Form.Label>
+                <Form.Control placeholder="add job" />
+              </Form.Group>
+
+              <Row className="mb-2">
+                <Form.Group as={Col}>
+                  <Form.Label>Skills</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    placeholder="add skills"
+                    name="skills"
+                    value={arrayToString(skills)}
+                    onChange={handleChange(setSkills)}
+                    style={{ height: "100px" }}
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} className="mb-4">
+                  <Form.Label>Languages</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    placeholder="add language"
+                    name="languages"
+                    value={arrayToString(languages)}
+                    onChange={handleChange(setLanguages)}
+                    style={{ height: "100px" }}
+                  />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Education</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="add education"
+                  name="education"
+                  value={arrayToString(education)}
+                  onChange={handleChange(setEducation)}
+                />
+              </Form.Group>
+
+              <div className="d-flex justify-content-end mt-4">
+                <Button variant="primary" type="submit" className="px-5 m-2">
+                  Save
+                </Button>
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
       </MainContainer>
     </Container>
   );
--- src/components/SettingsPage/SettingsPage.styled.jsx
+++ src/components/SettingsPage/SettingsPage.styled.jsx
@@ -2,33 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 45rem;
+  height: 100%;
   background: rgba(193, 193, 174, 0.09);
+  padding-bottom: 3rem;
 `;
 
 export const MainContainer = styled.div`
-  padding: 2.5rem 8rem;
+  width: 60rem;
+  heigh: 100%;
+  padding: 4rem 0 6rem;
 
   .card {
-    width: 43rem;
-    heigh: 404px;
+    width: 100%;
+    heigh: 100%;
     border-radius: 10px;
     padding: 1.5rem 1rem;
+    background: white;
+
+    .header {
+      margin: -20px 0 0 -20px;
+    }
   }
-`;
 
-export const Button = styled.button`
-  background: transparent;
-  color: black;
-  padding-left: 30px;
-  margin: 5px 0;
-  width: 311px;
-  height: 47px;
-  border: none;
-  text-align: left;
-
-  &:focus {
-    background: rgba(0, 128, 128, 0.5);
-    color: white;
+  @media (max-width: 900px) {
+    width: 40rem;
   }
 `;
