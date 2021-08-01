ea42c1185a5bf74a696552f4713cf2970cb3bfc6
Kizhan-Mohammed-karim <kizhan.m.kareem@gmail.com>
2021-08-01 18:25:57 +0300

    finished the form

--- src/components/AddServiecePage/AddService.jsx
+++ src/components/AddServiecePage/AddService.jsx
@@ -14,9 +14,11 @@ const MainContainer = styled.div`
   heigh: 100%;
   margin: 0 auto;
   padding: 4rem 0;
+  dislpay: flex;
+  align-items: center;
 
   .card {
-    width: 55rem;
+    width: 100%;
     heigh: 100%;
     border-radius: 10px;
     padding: 1.5rem 1rem;
@@ -26,6 +28,7 @@ const MainContainer = styled.div`
 // eslint-disable-next-line arrow-body-style
 const AddService = () => {
   const [images, setImages] = useState([]);
+  const [perks, setPerks] = useState([]);
 
   const handleChange = (set) => (e) => {
     set(e.target.value.split(", "));
@@ -33,7 +36,7 @@ const AddService = () => {
 
   const handleSave = (e) => {
     e.preventDefault();
-    console.log(images);
+    console.log(perks);
   };
 
   const arrayToString = (arr) => arr.join(", ");
@@ -45,28 +48,75 @@ const AddService = () => {
         <Card className="card shadow">
           <Card.Body>
             <Form onSubmit={handleSave}>
+              <Form.Group>
+                <Form.Label>Service Title</Form.Label>
+                <Form.Control placeholder="Tile" className="mb-4" name="title" />
+              </Form.Group>
+
               <Form.Group className="mb-3">
                 <Form.Label>
-                  Service Photo
+                  Service Images
                   <small className="text-muted mx-2">
                     Add images that best shaowcase your service
                   </small>
                 </Form.Label>
                 <Form.Control
                   as="textarea"
-                  placeholder="Service image links"
                   value={arrayToString(images)}
                   onChange={handleChange(setImages)}
                   name="images"
                 />
               </Form.Group>
+
               <Form.Group className="mb-3">
                 <Form.Label>
-                  Service Price
+                  Price
                   <small className="text-muted mx-2">Set a price for your service</small>
                 </Form.Label>
-                <Form.Control type="number" name="price" placeholder="Price" />
+                <Form.Control type="number" name="price" />
               </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Description
+                  <small className="text-muted mx-2">Briefly describe your service</small>
+                </Form.Label>
+                <Form.Control as="textarea" name="describtion" />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Details
+                  <small className="text-muted mx-2">Describe your service with details</small>
+                </Form.Label>
+                <Form.Control as="textarea" name="details" />
+              </Form.Group>
+
+              <Row className="mb-3">
+                <Form.Group as={Col}>
+                  <Form.Label>Delivery Time</Form.Label>
+                  <Form.Control type="number" name="delivery" />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Label>Number Of Revisions</Form.Label>
+                  <Form.Control type="number" name="price" />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3">
+                <Form.Label>
+                  Perks
+                  <small className="text-muted mx-2">Make your service more intresting</small>
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  value={arrayToString(perks)}
+                  onChange={handleChange(setPerks)}
+                  name="perks"
+                  style={{ height: "100px" }}
+                />
+              </Form.Group>
+
               <Button variant="primary" type="submit">
                 Save
               </Button>
