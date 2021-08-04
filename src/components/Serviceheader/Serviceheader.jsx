491bbebc8dd6edd32d70434c046d44d01da15248
dlzheen <dlzheenmahmood@gmail.com>
2021-08-04 07:13:45 -0700

    made seller component from updated develep branch to remove conflicts

---
+++ src/components/Serviceheader/Serviceheader.jsx
@@ -0,0 +1,28 @@
+
+import { Servicehead,Liststyle } from "./Serviceheader.styled";
+import Link from "next/link"
+const Serviceheader = ()=>{
+return (
+  
+   
+  <Servicehead>
+   
+  <ul>
+    <li>
+  <Link href="/service"><span>Overview</span></Link>
+   </li>
+   <li>
+  <Link href="/service/description"><span>Description</span></Link>
+   </li>
+   <li>
+  <Link href="/service/seller"><span>Seller</span></Link>
+   </li>
+   <li>
+  <Link href="/service/review"><span>Review</span></Link>
+   </li>
+</ul>
+
+</Servicehead>
+);
+}
+export default Serviceheader; 
---
+++ src/components/Serviceheader/Serviceheader.styled.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Servicehead = styled.div`
+padding-top:20px;
+padding-right:100px
+background-color:white;
+
+ul{
+    margin-right:20px;
+}
+ul li {
+    display: inline;
+    padding: 10px;
+    
+}
+ul li span{
+    text-decoration:none;
+    opacity: 0.5;
+}
+ul li span:hover {
+   opacity:1;
+   cursor:pointer;
+}
+
+`;
