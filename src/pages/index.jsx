5801962fac960e00f422b11b4c094bf9045e0f72
HasarAli <Hasar.Ali@protonmail.com>
2021-08-04 04:42:48 +0300

    removed merge conflict indicator

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-<<<<<<< HEAD
-import LargeProfileCard from "../components/LargeProfileCard/LargeProfileCard";
-// import { Navbar } from "@components/Navbar/Navbar";
-=======
->>>>>>> 6f07216eb0524b71d7ba1e0b40526c9c0c470c0b
 
 import { useAuthState } from "react-firebase-hooks/auth";
+import LargeProfileCard from "../components/LargeProfileCard/LargeProfileCard";
+// import { Navbar } from "@components/Navbar/Navbar";
+
 import {
   getAllFreelancers,
   getAllServices,
