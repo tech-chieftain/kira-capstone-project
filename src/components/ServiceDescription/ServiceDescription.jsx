a8a9c53c07b5331c3e04373c6e779ea15d8c271a
HasarAli <Hasar.Ali@protonmail.com>
2021-08-09 02:17:11 +0300

    fixed typo on seller description heading

--- src/components/ServiceDescription/ServiceDescription.jsx
+++ src/components/ServiceDescription/ServiceDescription.jsx
@@ -1,10 +1,9 @@
-import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import ReactMarkdown from "react-markdown";
 
 const ServiceDescription = ({ description }) => (
   <Card className="p-4 my-5">
-    <Card.Title as="h2">About services</Card.Title>
+    <Card.Title as="h2">About Service</Card.Title>
     <ReactMarkdown>{description}</ReactMarkdown>
   </Card>
 );
