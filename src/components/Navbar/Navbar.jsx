2791cadefb2b047cfe139e24d84d7045168f44e8
HasarAli <Hasar.Ali@protonmail.com>
2021-08-08 17:56:42 +0300

    added back responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,60 +39,64 @@ const NavBar = ({ overview, profilePicture, name, uid, handleLogOut }) => {
 
         <Searchbox />
 
-        {overview ? (
-          <Nav className="w-100 d-flex justify-content-end">
-            <LoginBtn onClick={openLoginModal}> Log In </LoginBtn>
-            <LoginModal
-              showLogin={showLogin}
-              setShowLogin={setShowLogin}
-              setShowSignup={setShowSignup}
-            />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          {overview ? (
+            <Nav className="w-100 d-flex justify-content-end">
+              <LoginBtn onClick={openLoginModal}> Log In </LoginBtn>
+              <LoginModal
+                showLogin={showLogin}
+                setShowLogin={setShowLogin}
+                setShowSignup={setShowSignup}
+              />
 
-            <JoinBtn onClick={openSignupModal}> Join </JoinBtn>
-            <SignupModal
-              showSignup={showSignup}
-              setShowSignup={setShowSignup}
-              setShowLogin={setShowLogin}
-            />
-          </Nav>
-        ) : (
-          <Nav className="w-75 d-flex justify-content-end">
-            <Nav.Link href="/">
-              <FaPlus size="25px" className="text-white mt-1" />
-            </Nav.Link>
-            <Nav.Link href="/">
-              <GrMail size="32px" className="text-white" />
-            </Nav.Link>
-            <Nav.Link href="/">
-              <IoIosNotifications size="32px" className="text-white" />
-            </Nav.Link>
+              <JoinBtn onClick={openSignupModal}> Join </JoinBtn>
+              <SignupModal
+                showSignup={showSignup}
+                setShowSignup={setShowSignup}
+                setShowLogin={setShowLogin}
+              />
+            </Nav>
+          ) : (
+            <Nav className="w-100 d-flex justify-content-end">
+              <Nav.Link href="/add_service">
+                <FaPlus size="25px" className="text-white mt-1" />
+              </Nav.Link>
+              <Nav.Link href="/">
+                <GrMail size="32px" className="text-white" />
+              </Nav.Link>
+              <Nav.Link href="/">
+                <IoIosNotifications size="32px" className="text-white" />
+              </Nav.Link>
 
-            <DropDown>
-              <Dropdown.Toggle className="dropdown-basic">
-                <FaUserAlt size="25px" className="text-white" />
-              </Dropdown.Toggle>
+              <DropDown>
+                <Dropdown.Toggle className="dropdown-basic">
+                  <FaUserAlt size="25px" className="text-white" />
+                </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                <Dropdown.Item href={`/profile/${uid}`}>
-                  {profilePicture ? (
-                    <ProfileImg src={profilePicture} roundedCircle fluid />
-                  ) : (
-                    <FaUserCircle size="28px" />
-                  )}
-                  <span className="mx-3 text-muted">{name}</span>
-                </Dropdown.Item>
-                <Dropdown.Item href="/settings">
-                  <MdSettings size="20px" />
-                  <span className="mx-3 text-muted">Settings</span>
-                </Dropdown.Item>
-                <Dropdown.Item onClick={handleLogOut}>
-                  <FiLogOut size="20px" />
-                  <span className="mx-3 text-muted">Log Out</span>
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </DropDown>
-          </Nav>
-        )}
+                <Dropdown.Menu>
+                  <Dropdown.Item href={`/profile/${uid}`}>
+                    {profilePicture ? (
+                      <ProfileImg src={profilePicture} roundedCircle fluid />
+                    ) : (
+                      <FaUserCircle size="28px" />
+                    )}
+                    <span className="mx-3 text-muted">{name}</span>
+                  </Dropdown.Item>
+
+                  <Dropdown.Item href="/settings">
+                    <MdSettings size="20px" />
+                    <span className="mx-3 text-muted">Settings</span>
+                  </Dropdown.Item>
+                  <Dropdown.Item onClick={handleLogOut}>
+                    <FiLogOut size="20px" />
+                    <span className="mx-3 text-muted">Log Out</span>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </DropDown>
+            </Nav>
+          )}
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
