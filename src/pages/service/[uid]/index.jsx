3ec086a3a953b67fba800bf12e56851d4d698d32
HasarAli <Hasar.Ali@protonmail.com>
2021-08-08 22:26:36 +0300

    dynamic paths and data fetching enabled

--- src/pages/service/[uid]/index.jsx
+++ src/pages/service/[uid]/index.jsx
@@ -1,10 +1,12 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Container from "react-bootstrap/Container";
+import PropTypes from "prop-types";
 import ServiceDescription from "../../../components/ServiceDescription/ServiceDescription";
 import ServiceHeader from "../../../components/ServiceHeader/ServiceHeader";
 import ServiceSeller from "../../../components/ServiceSeller/ServiceSeller";
+import { getService, getAllServicesUID, getUserInfo } from "../../../Utilities/FirebaseUtilities";
 
-const Service = () => (
+const Service = ({ service, user }) => (
   <>
     <Container>
       <ServiceHeader />
@@ -13,21 +15,46 @@ const Service = () => (
       <h5>Overview component will go here</h5>
     </Container>
     <Container id="description">
-      <ServiceDescription description="about this service" />
+      <ServiceDescription description={service.description} />
     </Container>
     <Container id="seller">
       <ServiceSeller
-        Name="John Doe"
-        Location="Tampa"
-        Description="About this man"
-        ProfilePic="https://picsum.photos/250"
+        Name={user.displayName}
+        Location={user.location}
+        Description={user.about}
+        ProfilePic={user.photoURL}
       />
     </Container>
   </>
 );
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common"])),
-  },
-});
+
+Service.propTypes = {
+  service: PropTypes.object,
+  user: PropTypes.object,
+};
+export const getStaticProps = async (context) => {
+  const service = await getService(context.params.uid);
+  const user = await getUserInfo({ uid: service.freelancerUID });
+
+  console.log("service:", service);
+  console.log("user:", user);
+
+  return {
+    props: {
+      service,
+      user,
+      ...(await serverSideTranslations(context.locale, ["common"])),
+    },
+  };
+};
+
+export async function getStaticPaths() {
+  const services = await getAllServicesUID();
+  const paths = services.map((service) => ({
+    params: { uid: service },
+  }));
+
+  return { paths, fallback: false };
+}
+
 export default Service;
